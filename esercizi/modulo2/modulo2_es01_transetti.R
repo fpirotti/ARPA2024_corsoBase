5e87c3880631990b54b331721ad241859327a33b
fpirotti <fpirotti@gmail.com>
2024-09-07 08:58:17 +0200

    update

--- esercizi/modulo2/modulo2_es01_transetti.R
+++ esercizi/modulo2/modulo2_es01_transetti.R
@@ -48,9 +48,51 @@ t2 %>% plot(add=T, col="red")
 
 
 t1.values <- terra::extractAlong(modisTemp, t1)
+
 plot(t1.values$mean, ylim=c(2,15), xlab="km",
      ylab="Temperatura °C",  type="l")
 t2.values <- terra::extractAlong(modisTemp, t2, online = T, bilinear = T)
 lines(t2.values$mean,  type="l", col="red" )
 
 
+
+
+
+nm <- length(t2)
+plot(t2[1:(nm-1)], t2[2:nm], pch="x", cex=0.5, xlab="km", ylab="Temperatura" )
+
+cc=1
+CovX<-cov(t2, t2)
+covs<-list("0"=CovX, "1"=cov(t2[1:(nm-1)], t2[2:nm] ) )
+
+
+for(lag in seq(6, 30, 5)){
+  message(lag)
+
+  covs[[as.character(lag)]]<-cov(t2[1:(nm-lag)], t2[(lag+1):nm])
+  points(t2[1:(nm-lag)], t2[(lag+1):nm], pch="x",cex=0.5, col=cc )
+  cc=cc+1
+
+}
+
+
+
+dati<- data.frame(Temperatura=t2, Distanza_Km=1:length(t2) )
+# modello.lm <- lm(data = dati)
+modello.lm <- lm(Temperatura~Distanza_Km, data = dati)
+summary(modello.lm)
+
+
+dati<- data.frame(Temperatura=t1, Distanza_Km=1:length(t1) )
+# modello.lm <- lm(data = dati)
+#
+modello.lm <- lm(Temperatura~ I(Distanza_Km*20), data = dati)
+
+summary(modello.lm)
+
+
+library(ggplot2)
+
+ggplot(dati, aes(x = Distanza_Km^2, y = Temperatura )) +
+  geom_point() +
+  stat_smooth(method = "lm") + theme_bw()
--- esercizi/modulo2/modulo2_es03_goSpatial.R
+++ esercizi/modulo2/modulo2_es03_goSpatial.R
@@ -2,16 +2,39 @@
 library(terra)
 library(tidyterra)
 
-temperature <- terra::rast("GeospatialApp/data/VenetoCorrectedMODIS_LST_Avg2017.tif")
+# Inserire il percorso CORRETTO di datindel vostro disco
+# NB se usate un progetto in RStudio la cartella di riferimento (radice)
+# da cui parte il percorso è quella del progetto!
+# E' ..........
+temperature <- terra::rast("esercizi/data/VenetoCorrectedMODIS_LST_Avg2017.tif")
 ## aggiungiamo una potenziale covariata - la quota!
-DEM <- terra::rast("GeospatialApp/data/VenetoDEM.tif")
-veneto <- sf::read_sf("GeospatialApp/data/veneto.gpkg")
+DEM <- terra::rast("esercizi/data/VenetoDEM.tif")
+# comuni del Veneto (non serve per ora)
+#veneto <- sf::read_sf("esercizi/data/veneto.gpkg")
 
 ## sample 1000 points
 smpl <- terra::spatSample(temperature, method="random",
                         size=1000, na.rm=T, as.points=T)
+## extract temperature
 q <- terra::extract(DEM, smpl, ID=F )
 smpl$altitude<- q$VenetoDEM
 
 plot(temperature)
 plot(smpl, pch="x", add=T)
+
+
+smpl$altitude <- terra::extract(DEM, smpl[,1:2], ID=F )[[1]]/1000
+smpl$altitudeM <- smpl$altitude *1000
+
+names(smpl)  <-  c("x","y","Temp", "Quota.km", "Quota.m")
+smpl<-as_tibble(smpl)
+
+plot(Temp~`Quota.km`, data = smpl)
+modello.lm <- lm(Temp~`Quota.km`, data = smpl)
+summary(modello.lm)
+
+
+plot(Temp~`Quota.m`, data = smpl)
+modello.lm <- lm(Temp~`Quota.m`, data = smpl)
+summary(modello.lm)
+
